3e5fb65c1d90764dfa42806f30b7d272cd06e875
Gamer20111 <91805736+Gamer20111@users.noreply.github.com>
2024-11-11 18:02:16 +0100

    dziala

--- OpenEngine3D/OpenEngine3D/main.cpp
+++ OpenEngine3D/OpenEngine3D/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <GL/glew.h>
 #include "Display.h"
-#include "Shader.h""
+#include "Shader.h"
 #include "Rigidbody.h"
 #include "Mesh.h"
 #include "Texture.h"
@@ -21,10 +21,13 @@ int main(void)
         Vertex(glm::vec3(0,0.5,0), glm::vec2(-0.5, -1.0)),
         Vertex(glm::vec3(0.5,-0.5,0), glm::vec2(0.0, 0.0)), };
 
-    Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+	unsigned int indices[] = { 0, 1, 2 };
+
+    Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]), indices, sizeof(indices)/sizeof(indices[0]));
+    Mesh mesh2("./res/monkey3.obj");
     Texture texture("./res/mcqueen.jpg");
     //Rigidbody rigidbody(0.0f, 5.0f, 0.0f);
-    Camera camera(glm::vec3(0, 0, -3), 70.0f, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
+    Camera camera(glm::vec3(0, 0, -5), 70.0f, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
 	Transform transform;
 
 	float counter = 0.0f;
@@ -47,7 +50,7 @@ int main(void)
         shader.Bind();
         texture.Bind(0);
         shader.Update(transform, camera);
-        mesh.Draw();
+        mesh2.Draw();
 
         //rigidbody.Update(0.006f);
         //rigidbody.Draw();
