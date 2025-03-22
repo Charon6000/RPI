88115c1926a55d2f03009d38b504a6e27430378a
Charon6000 <hubertwit2005@gmail.com>
2025-03-22 18:37:25 +0100

    odbijanie

--- OpenEngine3D/OpenEngine3D/GameManager.h
+++ OpenEngine3D/OpenEngine3D/GameManager.h
@@ -9,6 +9,7 @@ class GameManager {
 public:
     static std::vector<Object*> obiekty;
     void Update(Camera& camera);
+    void Simulate(bool czy);
 private:
     void CheckCollisions();
     void ObjectCompiling(Camera& camera);
